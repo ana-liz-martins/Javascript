78c3c7600fda0e432fad8c073ecc83b47971228d
Liz <anaah@live.co.uk>
2023-03-16 20:48:13 -0300

    Primeiros exercícios com estrutura While

---
+++ Aulajs While/ambiente.js
@@ -0,0 +1,18 @@
+
+
+var contador = 1 
+while (contador < 6) {
+    console.log('ODEIO ESSA ESTRUTURA')
+    contador++
+}
+
+// REPETIÇÃO COM TESTE LÓGICO NO INÍCIO
+
+
+var contador = 1 
+do {
+  console.log (`passo ${contador}`)
+  contador++
+} while (contador <= 6)
+
+// REPETIÇÃO COM TESTE LÓGICO NO FINAL
